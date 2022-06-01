bb0b56b3a5b9e0ce6e94b9e36901cc92911796ff
JinaKim <gina7441@gmail.com>
2022-06-01 17:43:09 +0100

    modify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {},
     module: {
         rules: [
                 {
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
+                    test: '/\.js$/',
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
                 }
         ]
     }
-    
 }
